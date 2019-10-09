25dc019faeadf3044b0b17d3c82a78c1a86f1224
Shaun Minnihan <spminnihan@gmail.com>
2019-10-09 16:08:43 -0500

    Added HTML updates

--- script.js
+++ script.js
@@ -12,21 +12,46 @@
 <img src="${}">
 */
 
-window.addEventListener("load", function() {
-   const form = document.getElementById("launchForm");
-   form.addEventListener("submit", function(event) {
+window.addEventListener("load", function () {
+   const formSubmit = document.getElementById("formSubmit");
+   formSubmit.addEventListener("click", function (event) {
+      // grab data from the form
       let pilotName = document.querySelector("input[name=pilotName");
       let copilotName = document.querySelector("input[name=copilotName");
       let fuelLevel = document.querySelector("input[name=fuelLevel");
       let cargoWeight = document.querySelector("input[name=cargoWeight");
-      if(pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoWeight.value === '') {
+      let faultyItems = document.getElementById("faultyItems");
+
+      if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoWeight.value === '') {
          alert("All fields required!");
          event.preventDefault();
-      }
-
-      if(isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoWeight.value))) {
+      } else if (isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoWeight.value))) {
          alert("Fuel level and Cargo weight must be numbers!");
          event.preventDefault();
+      } else {
+         let fuelStatus = 'high enough';
+         let cargoStatus = 'low enough';
+
+         if(fuelLevel.value < 10000) {
+            fuelStatus = 'too low';
+         }
+
+         if(cargoWeight.value > 10000) {
+            cargoStatus = 'too heavy';
+         }
+
+         faultyItems.innerHTML = `
+            <ol>
+               <li id="pilotStatus">Pilot ${pilotName.value} Ready</li>
+               <li id="copilotStatus">Co-pilot ${copilotName.value} Ready</li>
+               <li id="fuelStatus">Fuel ${fuelStatus} for launch</li>
+               <li id="cargoStatus">Cargo weight ${cargoStatus} for launch</li>
+            </ol>
+         `;
+         faultyItems.style.visibility = "visible";
+         event.preventDefault();
+
       }
+
    });
 });
