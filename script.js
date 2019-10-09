32539f4449ba7927960b9c593d1c9a0f1d4147b5
Shaun Minnihan <spminnihan@gmail.com>
2019-10-09 15:09:11 -0500

    added input validation

--- script.js
+++ script.js
@@ -11,3 +11,22 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function() {
+   const form = document.getElementById("launchForm");
+   form.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName");
+      let copilotName = document.querySelector("input[name=copilotName");
+      let fuelLevel = document.querySelector("input[name=fuelLevel");
+      let cargoWeight = document.querySelector("input[name=cargoWeight");
+      if(pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoWeight.value === '') {
+         alert("All fields required!");
+         event.preventDefault();
+      }
+
+      if(isNaN(Number(fuelLevel.value)) || isNaN(Number(cargoWeight.value))) {
+         alert("Fuel level and Cargo weight must be numbers!");
+         event.preventDefault();
+      }
+   });
+});
